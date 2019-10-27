e79a781ff0045afa3ff3888335c654bc2a2b85d2
Mohammed Amin KOULAL <mohammedkoulal@yahoo.fr>
2019-10-27 17:16:03 +0100

    modification d'une page

--- NUnitTestProject2/UnitTest1.cs
+++ NUnitTestProject2/UnitTest1.cs
@@ -1,18 +1,38 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
-namespace Tests
+namespace ToolsQA.Selenium_Basics
 {
-    public class Tests
+    class FindElementCommands
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        IWebDriver driver = new ChromeDriver(@"C:\Users\Utilisateur\Documents\Visual Studio 2015\Projects\Onlystore\packages\WebDriver.ChromeDriver.win32.76.0.3809.68\content");
 
         [Test]
-        public void Test1()
+        public void Test()
         {
-            Assert.Pass();
+
+            // Launch the ToolsQA WebSite
+            driver.Url = ("https://www.amazon.fr/");
+            driver.Manage().Window.Maximize();
+
+            // Type Name in the FirstName text box      
+            // Type Name in the FirstName text box      
+
+            driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("playstation 4");
+
+            //Type LastName in the LastName text box
+
+            // Click on the Submit button
+            driver.FindElement(By.XPath("(//*[contains(@value,'Go')])[1]")).Click();
+
+            //   driver.FindElement(By.XPath("(//*[contains(text(),'Livraison gratuite')])[1]")).Click();
+            Thread.Sleep(100);
+            driver.FindElement(By.XPath("(//*[contains(text(),'Call of Duty: Modern Warfare - Edition Exclusive Amazon (PS4)')])[1]")).Click();
+
+
         }
     }
 }
